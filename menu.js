f170488523e3f5767cb8af31c2039ebeb8ceec1b
Donmark2k <chukwuemekavp@gmail.com>
2022-12-19 20:54:32 +0100

    solve linters error

--- menu.js
+++ menu.js
@@ -1,18 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLogo = document.querySelector(".nav-branding");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navLogo = document.querySelector('.nav-branding');
 
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navLogo.classList.toggle('active');
+});
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    navLogo.classList.toggle("active");
-
-
-})
-
-document.querySelectorAll(".nav-link").forEach( n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+document.querySelectorAll('.nav-link').forEach((n) => n
+  .addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
