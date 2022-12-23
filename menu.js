8a0426e37a326e9dfd6f9db6d9f07ebcf83b2247
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2022-12-23 09:04:39 +0100

    Fix form validation

--- menu.js
+++ menu.js
@@ -176,3 +176,46 @@ for (let j = 0; j < projects.length; j += 1) {
     });
   });
 }
+
+// Form validation
+
+const form = document.getElementById('contact-me');
+const userName = document.getElementById('name');
+const email = document.getElementById('mail');
+const message = document.getElementById('msg');
+const error = document.getElementById('error-message');
+
+error.style.visibility = 'hidden';
+
+function formValidate(event) {
+  if (email.value !== email.value.toLowerCase()) {
+    error.style.visibility = 'visible';
+    error.innerHTML = 'Please input your email address in lowercase.';
+    event.preventDefault();
+  } else {
+    error.style.visibility = 'hidden';
+  }
+}
+
+form.addEventListener('submit', formValidate);
+
+function storeData() {
+  const user = {
+    UserName: userName.value,
+    UserEmail: email.value,
+    UserText: message.value,
+  };
+  localStorage.setItem('user', JSON.stringify(user));
+}
+
+userName.addEventListener('focusout', storeData);
+email.addEventListener('focusout', storeData);
+message.addEventListener('focusout', storeData);
+
+const data = JSON.parse(localStorage.getItem('user'));
+
+if (data) {
+  userName.value = data.UserName;
+  email.value = data.UserEmail;
+  message.value = data.UserText;
+}
