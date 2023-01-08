561b559bb2bc30d85fc4d78c9b1b9720811e7111
Donmark2k <chukwuemekavp@gmail.com>
2023-01-08 14:50:22 +0100

    array and event listeners practice

--- event.js
+++ event.js
@@ -1,78 +1,70 @@
-const nn = document.querySelector('.resumebutton');
+// const nn = document.querySelector('.resumebutton');
+// nn.addEventListener('clicked', (event) => {
+//   event.target.textContent = 'clicked';
+// },
+// false);
 
-nn.addEventListener ('clicked', (event) => {
-    console.log('onyeka');
-    event.target.textContent = 'clicked';
+// const ll = document.getElementById('error-message');
+// ll.style.display = 'none';
+// const resumeBtn = document.querySelector('.resumebutton');
+// const aboutMe = document.querySelector('.describeme');
+// resumeBtn.addEventListener('click', () => {
+// //   alert('Hello World!');
+//   //   aboutMe.style.display = 'none';
+//   aboutMe.style.color = 'orange';
+// });
 
-},
-false
-);
+// const email = document.getElementById('mail');
 
-const ll = document.getElementById('error-message')
-ll.style.display='none';
-const resumeBtn = document.querySelector('.resumebutton');
-const aboutMe = document.querySelector('.describeme');
-resumeBtn.addEventListener ('click', () => {
-    alert("Hello World!");
-//   aboutMe.style.display = 'none';
-  aboutMe.style.color = 'orange';
+// email.onfocus = function () {
+//   email.style.background = 'violet';
+// };
+// email.onblur = function () {
+//   email.style.background = 'white';
+// };
 
-});
+// const hh = document.querySelector('.last-paragraph h2');
+// hh.onmouseover = function () {
+//   hh.style.color = 'red';
+// };
 
+// hh.onmouseout = function () {
+//   hh.style.color = 'green';
+// };
 
-const email = document.getElementById('mail');
+// const cars = ['Saab', 'Volvo', 'BMW'];
+// var text = '';
+// for (let i = 0; i < cars.length; i++) {
+//   text += `${cars[i]} `;
+// }
+// console.log(text);
 
-email.onfocus = function() {
-    email.style.background = 'violet'
-}
-email.onblur = function() {
-    email.style.background = 'white'
-}
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
 
-const hh = document.querySelector('.last-paragraph h2');
-hh.onmouseover = function() {
-    hh.style.color = 'red';
-}
+// var fruits; var
+//   text;
+// fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
 
-hh.onmouseout = function() {
-    hh.style.color = 'green';
-}
+// text = '<ul>';
+// fruits.forEach(myFunction);
+// text += '</ul>';
 
-var cars = ["Saab","Volvo","BMW"];
-var text = "";
-for (let i=0; i< cars.length; i++ ){
-    text += cars[i] + " ";
+// console.log(text);
+// function myFunction(value) {
+//   text += `<li>${value}</li>`;
+// }
 
-}
-console.log(text);
+// var fruits; var text; let i; let
+//   car;
+// fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
 
-for (let i=0; i< cars.length; i++ ){
-    console.log(cars[i]);
-}
-
-
-var fruits, text;
-fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-text = "<ul>";
-fruits.forEach(myFunction);
-text += "</ul>";
-
-console.log(text)
-function myFunction(value) {
-  text += "<li>" + value + "</li>";
-}
-
-var fruits, text, i, car;
-fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-fruits.forEach ( function(fruit) {
-    console.log(fruit);
-
-});
-car = "";
-fruits.forEach ( function(i) {
-    car += i + " ";
-
-});
-console.log(car);
+// fruits.forEach((fruit) => {
+//   console.log(fruit);
+// });
+// car = '';
+// fruits.forEach((i) => {
+//   car += `${i} `;
+// });
+// console.log(car);
